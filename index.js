e626bb4882a591a4df56d357f9906aa6ec36a293
meronSIS <167541754+meronsisay@users.noreply.github.com>
2025-04-29 22:26:56 +0300

    added students profile fetch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // Import routes
 const authRoutes = require("./routes/authRoutes");
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,9 +1,10 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const JWT_SECRET_KEY = process.env.JWT_SECRET;
 
+
 const authenticate = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
-  
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
   if (!token) {
     return res.status(401).json({ error: "Authentication required" });
   }
@@ -11,8 +12,8 @@ const authenticate = (req, res, next) => {
   try {
     // Verify the token
     const decoded = jwt.verify(token, JWT_SECRET_KEY);
-    req.user = decoded;  // Attach decoded user info to request
-    next();  // Proceed to the next middleware/route handler
+    req.user = decoded; // Attach decoded user info to request
+    next(); // Proceed to the next middleware/route handler
   } catch (error) {
     return res.status(401).json({ error: "Invalid or expired token" });
   }
